75c2cc5cd31f257b390bea98b66e18998617aeb8
aajadshiva <133528082+aajadshiva@users.noreply.github.com>
2023-05-23 09:15:14 -0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://secret-project-main2.vercel.app/',
   credentials: true
 }));
 
